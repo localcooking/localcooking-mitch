2e283d5f5278728b8bc44e042bf0bfc5a46c6eb3
Athan Clark <athan.clark@gmail.com>
2018-04-20 12:54:54 -0600

    template update

--- frontend/src/Spec/Tag.purs
+++ frontend/src/Spec/Tag.purs
@@ -9,14 +9,18 @@ import React.DOM.Props as RP
 import React.Signal.WhileMounted as Signal
 import Data.UUID (GENUUID)
 import Data.URI (URI)
+import Data.Maybe (Maybe (..))
+import Data.Tuple (Tuple (..))
 import Data.URI.Location (Location)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Uncurried (mkEffFn1)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff, unsafePerformEff)
 import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Exception (EXCEPTION)
 
-import Crypto.Scrypt (SCRYPT)
+import MaterialUI.Chip (chip)
+import MaterialUI.Chip as Chip
 
 import IxSignal.Internal (IxSignal)
 import IxSignal.Internal as IxSignal
@@ -52,8 +56,39 @@ spec {label,onClick,onDelete} = T.simpleSpec performAction render
 
     render :: T.Render State Unit Action
     render dispatch props state children =
-      [ chip
-        { label: R.text "Three"
-        , onClick: mkEffFn1 \_ -> pure unit
-        }
+      [ Chip.withStyles
+        (\theme ->
+          { root:
+            { display: "flex"
+            , justifyContent: "center"
+            , flexWrap: "wrap"
+            , padding: theme.spacing.unit `div` 2
+            }
+          , chip:
+            { margin: theme.spacing.unit `div` 2
+            }
+          }
+        )
+        \{classes} ->
+        case Tuple onClick onDelete of
+          Tuple (Just onClick') (Just onDelete') -> chip
+            { label: R.text "Three"
+            , onClick: mkEffFn1 \_ -> onClick'
+            , onDelete: mkEffFn1 \_ -> onDelete'
+            , classes
+            }
+          Tuple Nothing (Just onDelete') -> chip
+            { label: R.text "Three"
+            , onDelete: mkEffFn1 \_ -> onDelete'
+            , classes
+            }
+          Tuple (Just onClick') Nothing -> chip
+            { label: R.text "Three"
+            , onClick: mkEffFn1 \_ -> onClick'
+            , classes
+            }
+          Tuple Nothing Nothing -> chip
+            { label: R.text "Three"
+            , classes
+            }
       ]
