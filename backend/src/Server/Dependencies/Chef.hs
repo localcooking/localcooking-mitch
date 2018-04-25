e687bc0200168e1e37d41270f650f75f1abac5e8
Athan Clark <athan.clark@gmail.com>
2018-04-24 19:37:00 -0600

    update

---
+++ backend/src/Server/Dependencies/Chef.hs
@@ -0,0 +1,10 @@
+module Server.Dependencies.Chef where
+
+import Types (AppM)
+
+import Web.Dependencies.Sparrow (Server, ServerContinue (..), ServerReturn (..))
+
+
+
+data ChefInitIn
+  = ChefInitInSetProfile
