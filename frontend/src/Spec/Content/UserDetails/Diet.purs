263fdedd0ba9bbf9922a74f67880df360e73cc59
Athan Clark <athan.clark@gmail.com>
2018-06-19 05:19:36 -0600

    add new button

--- frontend/src/Spec/Content/UserDetails/Diet.purs
+++ frontend/src/Spec/Content/UserDetails/Diet.purs
@@ -5,17 +5,26 @@ import LocalCooking.Spec.Tag (tag, TagVariant (TagDiet))
 import LocalCooking.Spec.Common.Form.Search as Search
 import LocalCooking.Spec.Common.Form.SearchResults as SearchResults
 import LocalCooking.Spec.Common.Form.Decisions as Decisions
+import LocalCooking.Spec.Common.Form.Submit as Submit
 
 import Prelude
+import Data.UUID (GENUUID)
 import Data.Maybe (Maybe (..))
 import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
 
 import Thermite as T
 import React (ReactElement, createClass, createElement) as R
 import React.DOM (text) as R
 
+import MaterialUI.Types (createStyles)
 import MaterialUI.Divider (divider)
+import MaterialUI.Typography (typography)
+import MaterialUI.Typography as Typography
+import MaterialUI.Grid (grid)
+import MaterialUI.Grid as Grid
+import MaterialUI.Button as Button
 
 import IxSignal.Internal (IxSignal)
 import IxSignal.Internal as IxSignal
@@ -36,6 +45,8 @@ type Action = Unit
 
 type Effects eff =
   ( ref :: REF
+  , exception :: EXCEPTION
+  , uuid :: GENUUID
   | eff)
 
 
@@ -44,6 +55,10 @@ spec :: forall eff
           { updatedQueue :: IxQueue (read :: READ) (Effects eff) Unit
           , signal :: IxSignal (Effects eff) String
           , setQueue :: One.Queue (write :: WRITE) (Effects eff) String
+          }
+        , submit ::
+          { triggerQueue :: IxQueue (read :: READ) (Effects eff) Unit
+          , disabled :: IxSignal (Effects eff) Boolean
           }
         , results ::
           { setQueue :: One.Queue (write :: WRITE) (Effects eff) (Array Diet)
@@ -58,22 +73,38 @@ spec :: forall eff
           }
         }
      -> T.Spec eff State Unit Action
-spec {search,results,decisions} = T.simpleSpec performAction render
+spec {search,submit,results,decisions} = T.simpleSpec performAction render
   where
     performAction action props state = pure unit
 
     render :: T.Render State Unit Action
     render dispatch props state children =
-      [ R.text "Diet"
+      [ typography
+        { variant: Typography.headline
+        } [R.text "Diet"]
       , divider {}
-      , Search.search
-        { label: R.text "Diet Name"
-        , fullWidth: true
-        , id: "diet-search"
-        , updatedQueue: search.updatedQueue
-        , searchSignal: search.signal
-        , setQueue: search.setQueue
-        }
+      , grid {spacing: Grid.spacing8, container: true}
+        [ grid {xs: 10, item: true}
+          [ Search.search
+            { label: R.text "Diet Name"
+            , fullWidth: true
+            , id: "diet-search"
+            , updatedQueue: search.updatedQueue
+            , searchSignal: search.signal
+            , setQueue: search.setQueue
+            }
+          ]
+        , grid {xs: 2, item: true}
+          [ Submit.submit
+            { color: Button.primary
+            , variant: Button.raised
+            , size: Button.medium
+            , style: createStyles {}
+            , triggerQueue: submit.triggerQueue
+            , disabledSignal: submit.disabled
+            } [R.text "Add"]
+          ]
+        ]
       , SearchResults.results
         { setQueue: results.setQueue
         , addQueue: results.addQueue
@@ -112,6 +143,10 @@ diet =
               , signal: searchSignal
               , setQueue: searchSetQueue
               }
+            , submit:
+              { triggerQueue: submitTriggerQueue
+              , disabled: submitDisabled
+              }
             , results:
               { setQueue: resultsSetQueue
               , addQueue: resultsAddQueue
@@ -138,3 +173,5 @@ diet =
     decisionsAddQueue = unsafePerformEff $ writeOnly <$> One.newQueue
     decisionsDelQueue = unsafePerformEff $ writeOnly <$> One.newQueue
     decisionsSignal = unsafePerformEff $ IxSignal.make []
+    submitTriggerQueue = unsafePerformEff $ readOnly <$> IxQueue.newIxQueue
+    submitDisabled = unsafePerformEff $ IxSignal.make true
